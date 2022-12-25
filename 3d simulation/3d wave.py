0014415bf51a9948ae9a5e8d51be2fc5027c9813
butayama <butayama@users.noreply.github.com>
2022-12-25 06:19:36 +0100

    change wave.py

--- 3d simulation/3d wave.py
+++ 3d simulation/3d wave.py
@@ -1,7 +1,6 @@
 import numpy as np
 import matplotlib.pyplot as plt
 
-
 # 常量
 pi = 3.1415926
 
@@ -21,7 +20,7 @@ R = 0.005
 
 # 两点换算为距离
 def r(x0, y0, z0, x1=0, y1=0, z1=0):
-    return np.sqrt(np.square(x0-x1) + np.square(y0-y1) + np.square(z0-z1))
+    return np.sqrt(np.square(x0 - x1) + np.square(y0 - y1) + np.square(z0 - z1))
 
 
 # 波运算函数
@@ -98,11 +97,11 @@ class F:
 # z = 0
 
 def f1(x):
-    return np.sqrt(np.square(_lambda)-np.square(x))
+    return np.sqrt(np.square(_lambda) - np.square(x))
 
 
 def f2(x):
-    return - np.sqrt(np.square(_lambda)-np.square(x))
+    return - np.sqrt(np.square(_lambda) - np.square(x))
 
 
 def g1(x, y):
@@ -110,7 +109,7 @@ def g1(x, y):
 
 
 def g2(x, y):
-    return H*_lambda/2
+    return H * _lambda / 2
 
 
 F(-_lambda, _lambda, f1, f2, g1)
@@ -126,10 +125,9 @@ for t in range(_Time_Split):
                 time = split_time * t
                 wave(_x, _y, _z, time)
 
-
 # 将矩阵乘以系数
-array_v_2 = A ** 2 * w ** 2 * array_v_2 / (points.len + 1)**2
-array_p_2 = rho**2 * u**2 * k**2 * array_v_2
+array_v_2 = A ** 2 * w ** 2 * array_v_2 / (points.len + 1) ** 2
+array_p_2 = rho ** 2 * u ** 2 * k ** 2 * array_v_2
 array_v_2 /= _Time_Split
 array_p_2 /= _Time_Split
 array_p_2_sqrt = np.sqrt(array_p_2)
@@ -140,24 +138,23 @@ def potential_energy(p, v_):
 
 
 # 声势能的计算公式
-array_U = 2 * pi * (R**3) * (array_p_2_sqrt / (3 * rho * (u**2)) - rho * array_v_2 / 2)
+array_U = 2 * pi * (R ** 3) * (array_p_2_sqrt / (3 * rho * (u ** 2)) - rho * array_v_2 / 2)
 
 # 梯度的计算式
 array_grad = np.gradient(array_U)
 
 print(array_grad)
 
-
 # 声压级
 p_rms = 2e-5
-array_level = 20 *np.log(np.abs(array_p_2_sqrt) / p_rms)
+array_level = 20 * np.log(np.abs(array_p_2_sqrt) / p_rms)
 
 # 下面的函数均用于绘制图形
 # 顺序是声压、声压级、声势能
 
 # 声压
 
-contour = plt.contourf(array_p_2_sqrt[_W//2, :, :])
+contour = plt.contourf(array_p_2_sqrt[_W // 2, :, :])
 
 plt.colorbar(contour)
 
@@ -167,7 +164,7 @@ plt.show()
 
 # 声压级
 
-contour = plt.contourf(array_level[_W//2, :, :])
+contour = plt.contourf(array_level[_W // 2, :, :])
 
 plt.colorbar(contour)
 
@@ -179,7 +176,7 @@ plt.show()
 
 # 声势能
 
-contour = plt.contourf(array_U[_W//2, :, :])
+contour = plt.contourf(array_U[_W // 2, :, :])
 
 plt.colorbar(contour)
 
